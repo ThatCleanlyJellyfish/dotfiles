40832da3a2a8a3a23126e2dad45f9450cb6143cf
Adrian Wheeldon <adrian@literal-labs.ai>
2024-09-26 16:11:11 +0100

    neovim: telescope: enable colorscheme preview

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -312,7 +312,7 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = { colorscheme = { enable_preview = true } },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
