a462e12f099523e93f0319608b0a1587c6f2f1ad
Adrian Wheeldon <adrian@literal-labs.ai>
2024-11-20 02:36:22 +0000

    neovim: diagnostic: add border

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -88,6 +88,11 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Diagnostic config
+vim.diagnostic.config {
+  float = { border = 'rounded' },
+}
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
