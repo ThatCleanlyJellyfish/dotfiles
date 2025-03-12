3881d88641c10db58dd55655e3f22cd6480b8bf0
Adrian Wheeldon <adrian@literal-labs.ai>
2025-03-12 00:53:32 +0000

    neovim(conform): python: use ruff_fix/ruff_format

--- neovim/.config/nvim/lua/plugins/lazy.lua
+++ neovim/.config/nvim/lua/plugins/lazy.lua
@@ -153,12 +153,13 @@ return {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        -- python = { 'ruff_fix', 'ruff_format' },
+        python = {
+          command = 'ruff',
+          args = { 'format', '-' },
+          stdin = true,
+        },
         rust = { 'rustfmt' },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
