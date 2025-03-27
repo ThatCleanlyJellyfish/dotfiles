23d065942adad4fb9ea89f7407b4b04aa639a848
Adrian Wheeldon <adrian@literal-labs.ai>
2025-03-27 14:58:30 +0000

    neovim(options): use cursorlineopt=number

--- neovim/.config/nvim/lua/options.lua
+++ neovim/.config/nvim/lua/options.lua
@@ -40,6 +40,7 @@ vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.inccommand = 'split'
 -- Show which line your cursor is on
 vim.opt.cursorline = true
+vim.opt.cursorlineopt = 'number'
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 -- Increase scrollback in terminals
