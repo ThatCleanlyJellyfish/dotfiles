6516931be6f012ccafc2af2243e6ea03f4ba565d
Adrian Wheeldon <adrian@literal-labs.ai>
2025-07-08 12:47:35 +0100

    neovim: blink.cmp: better icons in completion menu

--- neovim/.config/nvim/lua/plugins/auto-complete.lua
+++ neovim/.config/nvim/lua/plugins/auto-complete.lua
@@ -3,6 +3,8 @@ return { -- Autocompletion
   event = 'VimEnter',
   version = '1.*',
   dependencies = {
+    'onsails/lspkind.nvim',
+    'nvim-web-devicons',
     -- Snippet Engine
     {
       'L3MON4D3/LuaSnip',
@@ -72,6 +74,43 @@ return { -- Autocompletion
       -- By default, you may press `<c-space>` to show the documentation.
       -- Optionally, set `auto_show = true` to show the documentation after a delay.
       documentation = { auto_show = false, auto_show_delay_ms = 500 },
+      menu = {
+        draw = {
+          components = {
+            kind_icon = {
+              text = function(ctx)
+                local icon = ctx.kind_icon
+                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                  local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                  if dev_icon then
+                    icon = dev_icon
+                  end
+                else
+                  icon = require("lspkind").symbolic(ctx.kind, {
+                    mode = "symbol",
+                  })
+                end
+
+                return icon .. ctx.icon_gap
+              end,
+
+              -- Optionally, use the highlight groups from nvim-web-devicons
+              -- You can also add the same function for `kind.highlight` if you want to
+              -- keep the highlight groups in sync with the icons.
+              highlight = function(ctx)
+                local hl = ctx.kind_hl
+                if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                  local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                  if dev_icon then
+                    hl = dev_hl
+                  end
+                end
+                return hl
+              end,
+            }
+          }
+        }
+      }
     },
 
     sources = {
