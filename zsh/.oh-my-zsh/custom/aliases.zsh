528d139a9b460af309e79f0d241f04e7473a7b97
Adrian Wheeldon <adrian@literal-labs.ai>
2025-07-15 14:19:23 +0100

    zsh: aliases: git rebase origin/{develop,main}

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -8,6 +8,8 @@ alias ll='ls -lh'
 alias lla='ls -lAh'
 
 alias gch='git checkout'
+alias grbd='git rebase origin/$(git_develop_branch)'
+alias grbm='git rebase origin/$(git_main_branch)'
 
 alias pyv='. ./$1/.venv/bin/activate'
 
