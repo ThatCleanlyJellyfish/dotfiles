fef314176fd8b241ee64db4efb1e9decda2a11b6
Adrian Wheeldon <adrian@literal-labs.ai>
2025-10-09 20:21:43 +0100

    fix(zsh): oh-my-posh: rm _evalcache
    oh-my-posh doesn't work with _evalcache anymore. It starts instantly
    with the prescribed method anyway.

--- zsh/.zshrc.d/ohmyposh.zsh
+++ zsh/.zshrc.d/ohmyposh.zsh
@@ -1,5 +1,5 @@
 export PATH="$HOME/.local/bin:$PATH"
-_evalcache oh-my-posh init zsh --config $ZSH_CUSTOM/prompt.omp.yaml
+eval "$(oh-my-posh init zsh --config $ZSH_CUSTOM/prompt.omp.yaml)"
 # Pass the number of background jobs to oh-my-posh for the prompt.
 function set_poshcontext() {
   export BG_JOBS=$(jobs | wc -l | xargs)
